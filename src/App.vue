b6baadcb032b0da63d204d895b09836487e07a8e
zeed3v <felipebrionese@gmail.com>
2022-04-16 23:43:46 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contador">Contador</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="about">
-    <h1
+    <Titulo :contador="contador" :color="color" />
+<!--     <h1 
       :style="{'color': color}"
-    >Contador: {{contador}}</h1>
-    <button @click="aumentar">+</button>
-    <button @click="disminuir">-</button>
+    >Contador: {{contador}}</h1> -->
+<!--     <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button> -->
+    <Btn :textoBoton="'Aumentar'" @accion="aumentar"/>
+    <Btn :textoBoton="'Disminuir'" @accion="disminuir"/>
     <hr>
     <input type="text" v-model="texto">
     <p>{{texto}}</p>
@@ -12,9 +15,14 @@
 </template>
 
 <script>
+import Btn from '../components/Btn.vue'
+import Titulo from '../components/Titulo.vue'
 import { ref } from '@vue/reactivity'
 import { computed } from '@vue/runtime-core';
 export default {
+  components: {
+    Titulo, Btn
+  },
   setup(){
     const contador = ref(0);
     const texto = ref('')
